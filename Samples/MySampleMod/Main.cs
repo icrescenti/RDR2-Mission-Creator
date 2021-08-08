cf696528b686dfd44e60d6f4dd33658eac866451
terk <46972850+icrescenti@users.noreply.github.com>
2021-08-08 12:59:08 +0200

    sample

---
+++ Samples/MySampleMod/Main.cs
@@ -0,0 +1,33 @@
+﻿using RDR2;
+using System;
+using RDR2_Mission_Creator;
+
+namespace MySampleMod
+{
+    class Main : Script
+    {
+        public Main()
+        {
+            Tick += OnTick;
+            Interval = 1;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            //EXAMPLE IN PSEUDOCODE
+
+            /*
+             
+            if (game is loaded)
+                LevelManager.Load("red_dead_redemption");
+            
+            if (level has ended)
+                LevelManager.Load("finale1");
+
+            etc...
+
+             */
+        }
+
+    }
+}
